4a773e03b1b6c0dd00431f47752d67825727636d
zakariamakhroub <zakariamakhroub@gmail.com>
2026-07-08 04:16:26 +0100

    added data cleaning

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -1,11 +1,20 @@
+from preprocessing.data_cleaner import DataCleaner
+from preprocessing.data_explorer import DataExplorer
 from preprocessing.load_data import DataLoader
-
+import pandas as pd
 loader = DataLoader("data/raw/datasets")
 
 df = loader.load_turbine("turbine 0")
 
-print(df.head())
 
-print()
+cleaner = DataCleaner()
 
-print(df.info())
+
+clean_df = cleaner.clean(df)
+
+DataExplorer.explore(clean_df)
+
+print(clean_df.info())
+print(df["status_type_id"].value_counts())
+
+print(clean_df["train_test"].value_counts())
