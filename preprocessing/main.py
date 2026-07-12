d868cefe5e2171f8639b8c1dc2420611a4b9de62
zakariamakhroub <zakariamakhroub@gmail.com>
2026-07-12 09:45:38 +0100

    tarfet creator

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -2,6 +2,8 @@ from preprocessing.data_cleaner import DataCleaner
 from preprocessing.data_explorer import DataExplorer
 from preprocessing.load_data import DataLoader
 from preprocessing.data_splitter import DataSplitter
+from preprocessing.target_creator import TargetCreator
+
 import pandas as pd
 loader = DataLoader("data/raw/datasets")
 
@@ -10,13 +12,19 @@ df = loader.load_turbine("turbine 0")
 
 cleaner = DataCleaner()
 splitter = DataSplitter()
+target_creator = TargetCreator()
 
 
 clean_df = cleaner.clean(df)
 
-DataExplorer.explore(clean_df)
+
 
 train_df, prediction_df = splitter.split(clean_df)
 
+train_df = target_creator.create_target(train_df)
+prediction_df = target_creator.create_target(prediction_df)
+
+print(train_df["target"].value_counts())
 print("Training:", train_df.shape)
 print("Prediction:", prediction_df.shape)
+
